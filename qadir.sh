e53856fec374d983903b8bdc2ffb9a2ba054e1f9
hacker6291 <98105494+hacker6291@users.noreply.github.com>
2022-03-13 21:26:21 +0530

    Add files via upload

---
+++ qadir.sh
@@ -0,0 +1,267 @@
+echo "HACKING..." | lolcat
+sleep 2
+clear
+
+toilet -f future "QADIR AHMAD" | lolcat
+echo "-------------------------------------------------------" | lolcat
+echo   "Owner    : QADIR AHMAD $white" |lolcat
+echo   "Contact  : Join my Telegram Group $white" |lolcat
+echo   "Insta    : qadirahmad6291 $white" |lolcat
+echo "-------------------------------------------------------" | lolcat
+echo "                         CTRL + C To exit           " | lolcat
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  " Trying To Exit 🚪 ... "
+echo ""
+echo  "⚠️NOT FOR ILLEGEL USE⚠️"
+sleep 1
+echo ""
+echo "QADIR" | lolcat
+echo ""
+echo "BYE BYE 👋👋..." | lolcat
+echo ""
+echo "see u " | lolcat
+sleep 1
+exit
+}
+
+lagi=01
+while [ $lagi -lt 15 ];
+do
+echo ""
+echo -e "\e[1;101m\e[1;97m01\e[1;101m\e[0m\e[1;96m Zip_password_cracker\e[0m\n";
+echo -e "\e[1;101m\e[1;97m02\e[1;101m\e[0m\e[1;96m InstaBomber\e[0m\n";
+echo -e "\e[1;101m\e[1;97m03\e[1;101m\e[0m\e[1;96m password checker\e[0m\n";
+echo -e "\e[1;101m\e[1;97m04\e[1;101m\e[0m\e[1;96m Email Bomber\e[0m\n";
+echo -e "\e[1;101m\e[1;97m05\e[1;101m\e[0m\e[1;96m SMS Bomber\e[0m\n";
+echo -e "\e[1;101m\e[1;97m06\e[1;101m\e[0m\e[1;96m number_info\e[0m\n";
+echo -e "\e[1;101m\e[1;97m07\e[1;101m\e[0m\e[1;96m password cracker\e[0m\n";
+echo -e "\e[1;101m\e[1;97m08\e[1;101m\e[0m\e[1;96m Wifi hack\e[0m\n";
+echo -e "\e[1;101m\e[1;97m09\e[1;101m\e[0m\e[1;96m InstaUnfollow\e[0m\n";
+echo -e "\e[1;101m\e[1;97m10\e[1;101m\e[0m\e[1;96m userecon\e[0m\n";
+echo -e "\e[1;101m\e[1;97m11\e[1;101m\e[0m\e[1;96m phishing\e[0m\n";
+echo -e "\e[1;101m\e[1;97m12\e[1;101m\e[0m\e[1;96m location tracing\e[0m\n";
+echo -e "\e[1;101m\e[1;97m13\e[1;101m\e[0m\e[1;96m Android virus\e[0m\n";
+echo -e "\e[1;101m\e[1;97m14\e[1;101m\e[0m\e[1;96m TBomb\e[0m\n";
+echo -e "\e[1;101m\e[1;97m15\e[1;101m\e[0m\e[1;96m web_info\e[0m\n";
+echo -e "\e[1;101m\e[1;97m00\e[1;101m\e[0m\e[1;96m EXIT\e[0m\n";
+echo ""
+echo  "╭─QADIR" |lolcat
+read -p "╰──►" pil;
+
+
+#Zip pass cracker
+
+
+case $pil in
+01) clear
+toilet -f standard " Zip_password_cracker " -F gay
+git clone https://github.com/hacker6291/Zip_password_cracker.git
+pip install zipfile
+pip install os
+pip install colored
+cd Zip_password_cracker
+python3 Cracker.py
+ 
+
+;;
+
+
+#InstaBomber
+
+
+02) clear
+toilet -f standard " InstaBomber " -F gay
+git clone https://github.com/hacker6291/InstaBomber.git
+cd InstaBomber
+python3 crack.py
+
+
+;;
+
+
+#password checker
+
+
+03) clear
+toilet -f standard " password checker " -F gay
+git clone https://github.com/hacker6291/password-checker.git
+ls
+cd password-checker
+ls
+python3 checker.py
+
+
+;;
+
+
+#Email Bomber
+
+
+04) clear
+toilet -f standard " Email Bomber " -F gay
+git clone https://github.com/hacker6291/Email-Bombers.git
+cd Email-Bombers
+python3 bomber.py
+
+
+;;
+
+
+#SMS Bomber
+
+
+05) clear
+toilet -f standard "SMS Bomber" -F gay
+git clone https://github.com/hacker6291/SMS_Bomber.git
+cd Text_to_hash
+ls
+python3 converter.py
+
+
+
+;;
+
+
+#number_info
+
+
+06) clear
+toilet -f standard "number_info" -F gay
+ git clone https://github.com/hacker6291/number_info.git
+cd number_info
+python3 number.py
+
+
+
+;;
+
+
+#password cracker
+
+
+07) clear
+toilet -f standard "password cracker" -F gay
+git clone https://github.com/hacker6291/password-cracker.git
+cd password-cracker
+python3 hash.py
+;;
+
+
+#wifi
+
+
+08) clear
+toilet -f standard "wifi" -F gay
+git clone https://github.com/hacker6291/Wifi_Brutoforce.git
+cd Wifi_Brutoforce
+bash start.sh
+
+
+;;
+
+
+#Insta Unfollow
+
+
+09) clear
+toilet -f standard "InstaUnfollow" -F gay
+git clone https://github.com/hacker6291/InstaUnfollow.git
+cd InstaUnfollow
+ls
+bash start.sh
+
+
+;;
+
+
+#userecon
+
+
+10) clear
+toilet -f standard "userecon" -F gay
+git clone https://github.com/hacker6291/Userecon.git
+cd Userecon
+ls
+bash start.sh
+
+
+;;
+
+
+#phishing
+
+
+11) clear
+toilet -f standard "phishing Tool" -F gay
+git clone https://github.com/hacker6291/Phishing_tools.git
+cd Phishing_tools
+bash tool.sh
+;;
+
+
+#seeker
+
+
+12) clear
+toilet -f standard "Location tracking" -F gay
+git clone https://github.com/thewhiteh4t/seeker.py
+cd seeker/
+python3 seeker.py
+
+
+;;
+
+
+#virus
+
+
+13) clear
+toilet -f standard " TECH " -F gay
+git clone https://github.com/U-danbaiwa/Android-Virus
+cd Android-Virus
+python crash-phone.py
+;;
+
+
+#tbomb
+
+
+14) clear
+toilet -f standard "Qadir" -F gay
+git clone https://github.com/TheSpeedX/TBomb.git
+cd TBomb
+chmod +x TBomb.sh
+./TBomb.sh
+
+
+;;
+
+
+#web
+
+
+15) clear
+toilet -f standard "Web info" -F gay
+git clone https://github.com/skavngr/rapidscan.git
+cd rapidscan
+python3 rapidscan.py example.com
+
+
+;;
+
+
+00) echo "coded by : Hacker" | lolcat
+exit
+;;
+
+*) echo "Invalid, select a valid number" | lolcat
+esac
+done
+done
+
+=================="
